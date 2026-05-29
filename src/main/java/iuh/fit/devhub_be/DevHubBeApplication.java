d42ce8999abb98548cab1a6debb37b2146b8006d
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2026-05-29 09:24:10 +0700

    fix: update default timezone

--- src/main/java/iuh/fit/devhub_be/DevHubBeApplication.java
+++ src/main/java/iuh/fit/devhub_be/DevHubBeApplication.java
@@ -3,11 +3,13 @@ package iuh.fit.devhub_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DevHubBeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(DevHubBeApplication.class, args);
     }
-
 }
